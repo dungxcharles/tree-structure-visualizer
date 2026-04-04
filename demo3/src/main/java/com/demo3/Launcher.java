c3cbcb624d8cf4425924019e98b8a041547dc1aa
Tran Quang Thang <thangtranquang61@gmail.com>
2026-04-04 13:09:27 +0700

    Create repository

---
+++ demo3/src/main/java/com/demo3/Launcher.java
@@ -0,0 +1,9 @@
+package com.demo3;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
