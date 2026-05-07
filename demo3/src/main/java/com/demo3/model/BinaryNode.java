bbf19af70e11bc2b847bd65ec19f718078847217
Tran Quang Thang <thangtq111206@gmail.com>
2026-05-07 22:08:48 +0700

    Add constructor and getter/setter for BinaryNode class

--- demo3/src/main/java/com/demo3/model/BinaryNode.java
+++ demo3/src/main/java/com/demo3/model/BinaryNode.java
@@ -6,4 +6,28 @@ public class BinaryNode<T> extends Node<T>{
     private BinaryNode<T> right;
 
     
+    //constructor
+    public BinaryNode(T value){
+        super(value);
+    }
+
+    // getter 
+    public BinaryNode<T> getLeft(){
+        return this.left;
+    }
+    public BinaryNode<T> getRight(){
+        return this.right;
+    }
+
+    //setter
+    public void setLeft(BinaryNode<T> left){
+        this.left = left;
+    }
+
+    public void setRight(BinaryNode<T> right){
+        this.right = right;
+    }
+
+    // node only holds the data and collection -> Single responsibility principle 
+
 }
