2d8f7964d8575acebdea2be77d73159d7262e99c
Tran Quang Thang <thangtq111206@gmail.com>
2026-05-07 22:06:01 +0700

    Add Node class for tree structure

---
+++ demo3/src/main/java/com/demo3/model/Node.java
@@ -0,0 +1,18 @@
+package com.demo3.model;
+
+public abstract class Node<T> {
+    protected T value;
+    // protected for subclass
+
+    public Node(T value){
+        this.value = value;
+    }
+    public T getValue(){
+        return this.value;
+    }
+    public void setValue(T value){
+        this.value = value;
+    }
+    
+    
+}
