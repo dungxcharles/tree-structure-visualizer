4ca37a4f05bce684efe7d39d7f77cf481690e157
Tran Quang Thang <thangtq111206@gmail.com>
2026-05-08 10:20:01 +0700

    Fix the Red Black Node class

--- demo3/src/main/java/com/demo3/model/TreeNode/RBNode.java
+++ demo3/src/main/java/com/demo3/model/TreeNode/RBNode.java
@@ -1,5 +1,33 @@
-package com.demo3.model;
+package com.demo3.model.treenode;
 
-public class RBNode {
+public class RBNode <T extends Comparable<T>> extends BinaryNode<T>{
+    private RBNode<T> parent;
+    private Color color;
 
+    public enum Color{
+        RED,
+        BLACK;
+    }
+
+    public RBNode(T value){
+        super(value);
+        this.parent = null;
+        this.color = Color.RED;
+    }
+
+    // getter
+    // parent is needed in order to do red black tree manipulation
+    public RBNode<T> getParent(){
+        return this.parent;
+    }
+    public Color getColor(){
+        return this.color;
+    }
+    public void setParent(RBNode<T> parent){
+        this.parent = parent;
+    }
+    public void setColor(Color color){
+        this.color = color;
+    }
+    
 }
