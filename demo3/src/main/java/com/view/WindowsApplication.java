298ac58c4d695b744e6bed00fa34127ce20503c7
Mitsuhou <khiem.adolf@gmail.com>
2026-05-17 11:32:26 +0700

    feat: light mode pop up interface

--- demo3/src/main/java/com/view/WindowsApplication.java
+++ demo3/src/main/java/com/view/WindowsApplication.java
@@ -10,8 +10,8 @@ import java.io.IOException;
 public class WindowsApplication extends Application{
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/view/settings-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/view/tree-selection-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Help");
         stage.setScene(scene);
         stage.show();
