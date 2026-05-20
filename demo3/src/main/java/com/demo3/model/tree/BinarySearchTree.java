f0fab1b610baa9d983a1bd4e8fc20ea9962cfb5e
ToanVuz0111 <blockblack103@gmail.com>
2026-05-21 01:06:26 +0700

    add BST traversal operations

--- demo3/src/main/java/com/demo3/model/tree/BinarySearchTree.java
+++ demo3/src/main/java/com/demo3/model/tree/BinarySearchTree.java
@@ -1,5 +1,10 @@
 package com.demo3.model.tree;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
 import com.demo3.model.node.BinaryNode;
 
 
@@ -156,5 +161,85 @@ public abstract class BinarySearchTree<N extends BinaryNode> extends AbstractTre
         }
 
         return 1 + countNodes((N) node.getLeft()) + countNodes((N) node.getRight());
+    }
+
+    @Override
+    public List<Integer> traverse(TraversalType type) {
+        if (type == null) {
+            throw new IllegalArgumentException("Traversal type cannot be null.");
+        }
+
+        List<Integer> result = new ArrayList<>();
+        if (isEmpty()) {
+            return result;
+        }
+
+        switch (type) {
+            case IN_ORDER:
+                inOrderRec(this.root, result);
+                break;
+            case PRE_ORDER:
+                preOrderRec(this.root, result);
+                break;
+            case POST_ORDER:
+                postOrderRec(this.root, result);
+                break;
+            case BFS:
+                bfsTraverse(this.root, result);
+                break;
+        }
+
+        return result;
+    }
+
+    @SuppressWarnings("unchecked")
+    protected void inOrderRec(N node, List<Integer> result) {
+        if (node == null) {
+            return;
+        }
+
+        inOrderRec((N) node.getLeft(), result);
+        result.add(node.getValue());
+        inOrderRec((N) node.getRight(), result);
+    }
+
+    @SuppressWarnings("unchecked")
+    protected void preOrderRec(N node, List<Integer> result) {
+        if (node == null) {
+            return;
+        }
+
+        result.add(node.getValue());
+        preOrderRec((N) node.getLeft(), result);
+        preOrderRec((N) node.getRight(), result);
+    }
+
+    @SuppressWarnings("unchecked")
+    protected void postOrderRec(N node, List<Integer> result) {
+        if (node == null) {
+            return;
+        }
+
+        postOrderRec((N) node.getLeft(), result);
+        postOrderRec((N) node.getRight(), result);
+        result.add(node.getValue());
+    }
+
+    @SuppressWarnings("unchecked")
+    protected void bfsTraverse(N root, List<Integer> result) {
+        Queue<N> queue = new ArrayDeque<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            N current = queue.poll();
+            result.add(current.getValue());
+
+            if (current.getLeft() != null) {
+                queue.add((N) current.getLeft());
+            }
+            if (current.getRight() != null) {
+                queue.add((N) current.getRight());
+            }
+        }
     }
 }
