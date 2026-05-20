a595da1086c3a5f08b57a0a8e857d6977fbc3a81
ToanVuz0111 <blockblack103@gmail.com>
2026-05-21 00:48:00 +0700

    add BST insertion logic

--- demo3/src/main/java/com/demo3/model/tree/BinarySearchTree.java
+++ demo3/src/main/java/com/demo3/model/tree/BinarySearchTree.java
@@ -2,6 +2,7 @@ package com.demo3.model.tree;
 
 import com.demo3.model.node.BinaryNode;
 
+
 public abstract class BinarySearchTree<N extends BinaryNode> extends AbstractTree<N> {
 
     protected abstract N createNode(int value);
@@ -13,4 +14,33 @@ public abstract class BinarySearchTree<N extends BinaryNode> extends AbstractTre
         }
         this.root = createNode(value);
     }
+
+    @Override
+    public boolean insert(int parentValue, int value) {
+        return insert(value);
+    }
+
+    public boolean insert(int value) {
+        if (search(value)) {
+            return false;
+        }
+
+        this.root = insertRec(this.root, createNode(value));
+        return true;
+    }
+
+    @SuppressWarnings("unchecked")
+    protected N insertRec(N current, N newNode) {
+        if (current == null) {
+            return newNode;
+        }
+
+        if (newNode.getValue() < current.getValue()) {
+            current.setLeft(insertRec((N) current.getLeft(), newNode));
+        } else if (newNode.getValue() > current.getValue()) {
+            current.setRight(insertRec((N) current.getRight(), newNode));
+        }
+
+        return current;
+    }   
 }
