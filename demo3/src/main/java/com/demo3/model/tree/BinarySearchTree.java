0091612bd1bb1af95b68e20a45c768142511c5c8
ToanVuz0111 <blockblack103@gmail.com>
2026-05-21 00:53:17 +0700

    add BST deletion logic

--- demo3/src/main/java/com/demo3/model/tree/BinarySearchTree.java
+++ demo3/src/main/java/com/demo3/model/tree/BinarySearchTree.java
@@ -5,6 +5,8 @@ import com.demo3.model.node.BinaryNode;
 
 public abstract class BinarySearchTree<N extends BinaryNode> extends AbstractTree<N> {
 
+    private boolean deleted;
+
     protected abstract N createNode(int value);
 
     @Override
@@ -42,5 +44,59 @@ public abstract class BinarySearchTree<N extends BinaryNode> extends AbstractTre
         }
 
         return current;
-    }   
+    }
+
+    @Override
+    public boolean delete(int value) {
+        deleted = false;
+        this.root = deleteRec(this.root, value);
+        return deleted;
+    }
+
+    @SuppressWarnings("unchecked")
+    protected N deleteRec(N current, int value) {
+        if (current == null) {
+            return null;
+        }
+
+        if (value < current.getValue()) {
+            current.setLeft(deleteRec((N) current.getLeft(), value));
+            return current;
+        }
+        if (value > current.getValue()) {
+            current.setRight(deleteRec((N) current.getRight(), value));
+            return current;
+        }
+
+        deleted = true;
+        if (current.getLeft() == null) {
+            return (N) current.getRight();
+        }
+        if (current.getRight() == null) {
+            return (N) current.getLeft();
+        }
+
+        N successor = minimum((N) current.getRight());
+        current.setValue(successor.getValue());
+        current.setRight(deleteMinimum((N) current.getRight()));
+        return current;
+    }
+
+    @SuppressWarnings("unchecked")
+    protected N deleteMinimum(N current) {
+        if (current.getLeft() == null) {
+            return (N) current.getRight();
+        }
+
+        current.setLeft(deleteMinimum((N) current.getLeft()));
+        return current;
+    }
+
+    @SuppressWarnings("unchecked")
+    protected N minimum(N node) {
+        while (node.getLeft() != null) {
+            node = (N) node.getLeft();
+        }
+        return node;
+    }
 }
