7a9d569aeed6578d3d0d3557831577e713459857
ToanVuz0111 <blockblack103@gmail.com>
2026-05-21 10:07:06 +0700

    add HeapTree lifecycle helpers

--- demo3/src/main/java/com/demo3/model/tree/HeapTree.java
+++ demo3/src/main/java/com/demo3/model/tree/HeapTree.java
@@ -12,4 +12,37 @@ public class HeapTree extends AbstractTree<BinaryNode> {
     public HeapTree() {
         this.heap = new ArrayList<>();
     }
+
+    @Override
+    public void create(int value) {
+        if (!isEmpty()) {
+            return;
+        }
+
+        heap.add(new BinaryNode(value));
+        rebuildLinks();
+    }
+
+    @Override
+    public int getNumberOfNodes() {
+        return heap.size();
+    }
+
+    @Override
+    public void clear() {
+        heap.clear();
+        this.root = null;
+    }
+
+    private int parentIndex(int index) {
+        return (index - 1) / 2;
+    }
+
+    private int leftIndex(int index) {
+        return 2 * index + 1;
+    }
+
+    private int rightIndex(int index) {
+        return 2 * index + 2;
+    }
 }
