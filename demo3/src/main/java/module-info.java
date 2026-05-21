a1161afd6c164d19b2a44c9a944a37dcc2e1441f
DungxCharles <nguyentiendungok@gmail.com>
2026-05-21 16:37:12 +0700

    fix: edit files for UI to run

--- demo3/src/main/java/module-info.java
+++ demo3/src/main/java/module-info.java
@@ -1,10 +1,8 @@
-module com.demo3 {
+module com.view {
     requires javafx.controls;
     requires javafx.fxml;
 
 
-    opens com.demo3 to javafx.fxml;
-    exports com.demo3;
     opens com.view to javafx.fxml;
     exports com.view;
 }
