89d3fb84530cec8c2cdc078f90189f7a1934f311
ToanVuz0111 <blockblack103@gmail.com>
2026-05-21 09:02:45 +0700

    implement insert and recursive insertion methods in AVLTree

--- demo3/src/main/java/com/demo3/model/tree/AVLTree.java
+++ demo3/src/main/java/com/demo3/model/tree/AVLTree.java
@@ -11,4 +11,30 @@ public class AVLTree extends BinarySearchTree<AVLNode> {
     protected AVLNode createNode(int value) {
         return new AVLNode(value);
     }
+
+    @Override
+    public boolean insert(int value) {
+        if (search(value)) {
+            return false;
+        }
+
+        inserted = false;
+        this.root = insertRec(this.root, value);
+        return inserted;
+    }
+
+    private AVLNode insertRec(AVLNode node, int value) {
+        if (node == null) {
+            inserted = true;
+            return createNode(value);
+        }
+
+        if (value < node.getValue()) {
+            node.setLeft(insertRec(node.getLeft(), value));
+        } else if (value > node.getValue()) {
+            node.setRight(insertRec(node.getRight(), value));
+        }
+
+        return rebalance(node);
+    }
 }
