ab1e17c40fd27d8ec7c2d21adb207de581bc81d9
Mitsuhou <khiem.adolf@gmail.com>
2026-06-01 21:26:08 +0700

    feat: add general tree layout

---
+++ demo3/src/main/java/com/visualization/layout/GeneralTreeLayout.java
@@ -0,0 +1,10 @@
+package com.visualization.layout;
+
+import com.visualization.model.VisualTree;
+
+public class GeneralTreeLayout implements LayoutStrategy {
+    public GeneralTreeLayout() {}
+
+    @Override
+    public void calculateLayout(VisualTree visualTree, double containerWidth, double containerHeight) {}
+}
