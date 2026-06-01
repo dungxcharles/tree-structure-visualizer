9ffbb304d6f7e9eaeaaa3d284081bef355ad8c5c
Mitsuhou <khiem.adolf@gmail.com>
2026-06-01 21:36:01 +0700

    feat: Node renderer: set default radius for node

--- demo3/src/main/java/com/visualization/view/render/DefaultNodeRenderer.java
+++ demo3/src/main/java/com/visualization/view/render/DefaultNodeRenderer.java
@@ -1,10 +1,18 @@
 package com.visualization.view.render;
 
 import com.visualization.model.VisualNode;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 
 public class DefaultNodeRenderer<T> implements NodeRenderer<T> {
-    public DefaultNodeRenderer() {}
+    private static final double RADIUS = 20.0;
+
+    public DefaultNodeRenderer() {
+    }
 
     @Override
-    public void render(T graphicsContext, VisualNode node) {}
+    public void render(T graphicsContext, VisualNode node) {
+
+    }
 }
