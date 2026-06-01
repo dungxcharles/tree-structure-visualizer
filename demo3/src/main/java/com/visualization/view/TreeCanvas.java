5d05bca191e82b8b05c43dd9ffc61252aa975300
Mitsuhou <khiem.adolf@gmail.com>
2026-06-01 22:16:05 +0700

    feat: remove the use of generic in treecanvas

--- demo3/src/main/java/com/visualization/view/TreeCanvas.java
+++ demo3/src/main/java/com/visualization/view/TreeCanvas.java
@@ -1,15 +1,19 @@
 package com.visualization.view;
 
+import com.visualization.model.VisualEdge;
+import com.visualization.model.VisualNode;
 import com.visualization.model.VisualTree;
-import com.visualization.view.render.NodeRenderer;
 import com.visualization.view.render.EdgeRenderer;
+import com.visualization.view.render.NodeRenderer;
+import javafx.scene.canvas.GraphicsContext;
 
-public class TreeCanvas<T> {
-    private NodeRenderer<T> nodeRenderer;
-    private EdgeRenderer<T> edgeRenderer;
+public class TreeCanvas {
+    private NodeRenderer<GraphicsContext> nodeRenderer;
+    private EdgeRenderer<GraphicsContext> edgeRenderer;
 
-    public TreeCanvas(NodeRenderer<T> nodeRenderer, EdgeRenderer<T> edgeRenderer) {}
+    public TreeCanvas(NodeRenderer<GraphicsContext> nodeRenderer, EdgeRenderer<GraphicsContext> edgeRenderer) {
+        this.nodeRenderer = nodeRenderer;
+        this.edgeRenderer = edgeRenderer;
+    }
 
-    public void draw(VisualTree tree, T graphicsContext) {}
-    public void clear(T graphicsContext) {}
 }
