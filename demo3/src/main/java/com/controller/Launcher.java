51e1f8d6a1d2b05983ac389bc7d343ede49d0fd4
Mitsuhou <khiem.adolf@gmail.com>
2026-06-01 16:46:43 +0700

    feat: modify launcher for workspace use

--- demo3/src/main/java/com/controller/Launcher.java
+++ demo3/src/main/java/com/controller/Launcher.java
@@ -2,9 +2,21 @@ package com.controller;
 
 import com.controller.mainmenu.MainMenuApplication;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class Launcher {
+public class Launcher extends Application {
     public static void main(String[] args) {
-        Application.launch(MainMenuApplication.class, args);
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(MainMenuApplication.class.getResource("/com/view/workspace.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Help");
+        stage.setScene(scene);
+        stage.show();
     }
 }
