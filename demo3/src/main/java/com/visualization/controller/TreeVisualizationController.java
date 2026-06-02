af4fda68bf8cf52872a492eeb70fc6c4a8e171ed
Mitsuhou <khiem.adolf@gmail.com>
2026-06-02 19:53:10 +0700

    feat: initialize the code for controller

--- demo3/src/main/java/com/visualization/controller/TreeVisualizationController.java
+++ demo3/src/main/java/com/visualization/controller/TreeVisualizationController.java
@@ -5,21 +5,58 @@ import com.visualization.view.TreeCanvas;
 import com.visualization.view.animation.AnimationManager;
 import com.visualization.layout.LayoutStrategy;
 
-public class TreeVisualizationController<T> {
+import javafx.scene.canvas.GraphicsContext;
+
+public class TreeVisualizationController {
     private VisualTree visualTree;
-    private TreeCanvas<T> canvas;
+    private TreeCanvas canvas;
     private AnimationManager animationManager;
     private LayoutStrategy layoutStrategy;
 
     public TreeVisualizationController(
-            TreeCanvas<T> canvas, 
+            TreeCanvas canvas, 
             AnimationManager animationManager, 
-            LayoutStrategy layoutStrategy) {}
+            LayoutStrategy layoutStrategy) {
+        this.visualTree = new VisualTree();
+        this.canvas = canvas;
+        this.animationManager = animationManager;
+        this.layoutStrategy = layoutStrategy;
+    }
 
-    public void setTreeData(Object logicalTreeData) {}
-    public void updateLayout(double width, double height) {}
-    public void renderFrame(T graphicsContext) {}
-    public void animateNodeInsertion(Object logicalNodeInfo) {}
-    public void animateNodeDeletion(Object logicalNodeInfo) {}
-    public void setLayoutStrategy(LayoutStrategy layoutStrategy) {}
+    public void setTreeData(Object logicalTreeData) {
+        // TODO: Map the abstract logicalTreeData to VisualTree nodes and edges.
+        // For now, simply clear the visual tree when new data is injected.
+        if (this.visualTree != null) {
+            this.visualTree.clear();
+        }
+    }
+
+    public void updateLayout(double width, double height) {
+        if (this.layoutStrategy != null && this.visualTree != null) {
+            this.layoutStrategy.calculateLayout(this.visualTree, width, height);
+        }
+    }
+
+    public void renderFrame(GraphicsContext graphicsContext) {
+        if (this.canvas != null && graphicsContext != null) {
+            this.canvas.clear(graphicsContext);
+            this.canvas.draw(this.visualTree, graphicsContext);
+        }
+    }
+
+    public void animateNodeInsertion(Object logicalNodeInfo) {
+        // TODO: Convert logicalNodeInfo to VisualNode, calculate layout delta, and pass to animationManager
+    }
+
+    public void animateNodeDeletion(Object logicalNodeInfo) {
+        // TODO: Find VisualNode from logicalNodeInfo, create disappearing animation, and pass to animationManager
+    }
+
+    public void setLayoutStrategy(LayoutStrategy layoutStrategy) {
+        this.layoutStrategy = layoutStrategy;
+    }
+
+    public VisualTree getVisualTree() {
+        return visualTree;
+    }
 }
