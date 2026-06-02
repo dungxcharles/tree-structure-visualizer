208797c95f53fba7225a2895e7ce33d810c6eb97
DungxCharles <nguyentiendungok@gmail.com>
2026-06-02 21:06:19 +0700

    feat: add mapLogicalNodeToVisual method in TreeVisualizationController

--- demo3/src/main/java/com/visualization/controller/TreeVisualizationController.java
+++ demo3/src/main/java/com/visualization/controller/TreeVisualizationController.java
@@ -44,6 +44,40 @@ public class TreeVisualizationController {
         }
     }
 
+    private VisualNode mapLogicalNodeToVisual(com.model.node.Node logicalNode, VisualNode parentVisual) {
+        if (logicalNode == null) return null;
+
+        String id = String.valueOf(System.identityHashCode(logicalNode));
+        String label = String.valueOf(logicalNode.getValue());
+        VisualNode vNode = new VisualNode(id, label);
+
+        if (logicalNode instanceof com.model.node.RBNode) {
+            com.model.node.RBNode rbNode = (com.model.node.RBNode) logicalNode;
+            vNode.setColorHex(rbNode.getColor() == com.model.node.RBNode.Color.RED ? "#ff0000" : "#333333");
+        } else {
+            vNode.setColorHex("#ffffff");
+        }
+
+        this.visualTree.addNode(vNode);
+
+        if (parentVisual != null) {
+            this.visualTree.addEdge(new VisualEdge(parentVisual, vNode));
+        }
+
+        if (logicalNode instanceof com.model.node.BinaryNode) {
+            com.model.node.BinaryNode bNode = (com.model.node.BinaryNode) logicalNode;
+            if (bNode.getLeft() != null) mapLogicalNodeToVisual(bNode.getLeft(), vNode);
+            if (bNode.getRight() != null) mapLogicalNodeToVisual(bNode.getRight(), vNode);
+        } else if (logicalNode instanceof com.model.node.GenericNode) {
+            com.model.node.GenericNode gNode = (com.model.node.GenericNode) logicalNode;
+            for (com.model.node.GenericNode child : gNode.getChildren()) {
+                mapLogicalNodeToVisual(child, vNode);
+            }
+        }
+
+        return vNode;
+    }
+
     public void updateLayout(double width, double height) {
         if (this.layoutStrategy != null && this.visualTree != null) {
             this.layoutStrategy.calculateLayout(this.visualTree, width, height);
