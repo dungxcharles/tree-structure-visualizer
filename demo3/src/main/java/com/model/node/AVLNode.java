531d64c09d05e5858ebd52d65482bf0de4eeb6f3
DungxCharles <nguyentiendungok@gmail.com>
2026-06-02 21:32:24 +0700

    feat: add AVLNode class in model

---
+++ demo3/src/main/java/com/model/node/AVLNode.java
@@ -0,0 +1,53 @@
+package com.demo3.model.node;
+
+public class AVLNode extends BinaryNode {
+
+    private int height;
+
+    public AVLNode(int value) {
+        super(value);
+        this.height = 1;
+    }
+
+    @Override
+    public AVLNode getLeft() {
+        return (AVLNode) super.getLeft();
+    }
+
+    @Override
+    public AVLNode getRight() {
+        return (AVLNode) super.getRight();
+    }
+
+    @Override
+    public void setLeft(BinaryNode left) {
+        if (left != null && !(left instanceof AVLNode)) {
+            throw new IllegalArgumentException("AVLNode can only use AVLNode children.");
+        }
+        super.setLeft(left);
+    }
+
+    @Override
+    public void setRight(BinaryNode right) {
+        if (right != null && !(right instanceof AVLNode)) {
+            throw new IllegalArgumentException("AVLNode can only use AVLNode children.");
+        }
+        super.setRight(right);
+    }
+
+    public void setLeft(AVLNode left) {
+        super.setLeft(left);
+    }
+
+    public void setRight(AVLNode right) {
+        super.setRight(right);
+    }
+
+    public int getStoredHeight() {
+        return this.height;
+    }
+
+    public void setStoredHeight(int height) {
+        this.height = height;
+    }
+}
