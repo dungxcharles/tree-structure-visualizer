34a213c36129fd99dc6e94b89d3a778a954f7d06
DungxCharles <nguyentiendungok@gmail.com>
2026-06-03 10:05:27 +0700

    feat: add NavigationManager class to switch scene

---
+++ demo3/src/main/java/com/controller/NavigationManager.java
@@ -0,0 +1,43 @@
+package com.controller;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class NavigationManager {
+
+    private Stage stage;
+    private static NavigationManager instance;
+
+    private NavigationManager() {}
+
+    public static NavigationManager getInstance() {
+        if (instance == null) {
+            instance = new NavigationManager();
+        }
+        return instance;
+    }
+
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
+
+    public void navigateTo(String fxmlPath) {
+        if (stage == null) {
+            System.err.println("Stage is not set in NavigationManager.");
+            return;
+        }
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlPath));
+            Parent root = loader.load();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
