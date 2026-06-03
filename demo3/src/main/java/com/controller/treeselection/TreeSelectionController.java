00e53fb357fffc97b7ddd45882b0d43884d35968
DungxCharles <nguyentiendungok@gmail.com>
2026-06-03 21:46:01 +0700

    feat: update controller of credit window and tree selection window

--- demo3/src/main/java/com/controller/treeselection/TreeSelectionController.java
+++ demo3/src/main/java/com/controller/treeselection/TreeSelectionController.java
@@ -1,18 +1,48 @@
 package com.controller.treeselection;
 
 import com.controller.NavigationManager;
+import javafx.beans.binding.Bindings;
+import javafx.beans.binding.DoubleBinding;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class TreeSelectionController {
+
+    @FXML
+    private AnchorPane mainContainer;
+
     @FXML
     private Button btnBack;
+
+    private static final double DESIGN_WIDTH = 1280.0;
+    private static final double DESIGN_HEIGHT = 720.0;
+
+    @FXML
+    public void initialize() {
+        mainContainer.sceneProperty().addListener((observable, oldScene, newScene) -> {
+            if (newScene != null) {
+                applyResponsiveScaling(newScene);
+            }
+        });
+    }
+
+    private void applyResponsiveScaling(Scene scene) {
+        DoubleBinding scaleBinding = Bindings.createDoubleBinding(() -> {
+            double scaleX = scene.getWidth() / DESIGN_WIDTH;
+            double scaleY = scene.getHeight() / DESIGN_HEIGHT;
+            return Math.min(scaleX, scaleY);
+        }, scene.widthProperty(), scene.heightProperty());
+
+        mainContainer.scaleXProperty().bind(scaleBinding);
+        mainContainer.scaleYProperty().bind(scaleBinding);
+    }
 
     @FXML
     public void initWorkspace() throws IOException {
